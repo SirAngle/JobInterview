cd694feb466d3f6b61815d94e9a8f3746221c223
Angle <sirangle@protonmail.com>
2021-12-04 21:23:55 +0200

    2nd Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TopbarComponent } from './topbar/topbar.component';
-import { MainbodyComponent } from './mainbody/mainbody.component';
-import { WrapperComponent } from './wrapper/wrapper.component';
+import { TopbarComponent } from '../app/components/topbar/topbar.component';
+import { MainbodyComponent } from '../app/components/mainbody/mainbody.component';
+import { WrapperComponent } from '../app/components/wrapper/wrapper.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FloatingWrapperComponent } from './floating-wrapper/floating-wrapper.component';
-import { JoinNowComponent } from './join-now/join-now.component';
+import { FloatingWrapperComponent } from '../app/components/floating-wrapper/floating-wrapper.component';
 import { AvailableMoneyTradeComponent } from './pages/available-money-trade/available-money-trade.component';
 import { JoinComponent } from './pages/join/join.component';
 
@@ -19,16 +18,11 @@ import { JoinComponent } from './pages/join/join.component';
     MainbodyComponent,
     WrapperComponent,
     FloatingWrapperComponent,
-    JoinNowComponent,
     AvailableMoneyTradeComponent,
-    JoinComponent
+    JoinComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FontAwesomeModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
